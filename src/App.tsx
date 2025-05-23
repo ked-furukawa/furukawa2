f9072c73a3d08c32932147bf9ba6b03bdc66e558
tk-kurosawa <tk_kurosawa@trust-coms.com>
2025-05-23 10:37:14 +0900

    App.tsxをOrderテーブルに合わせて暫定処置を行いました

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,40 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Order"]["type"]>>([]);
+  const [orders, setOrders] = useState<Array<Schema["Order"]["type"]>>([]); //テーブルを変更したのでエラーが出ないような暫定対応、todoという変数をorderに変更してます
 
   useEffect(() => {
     client.models.Order.observeQuery().subscribe({
-      next: (Order) => setTodos([...Order.items]),
+      next: ({items}) => {
+        setOrders([...items]);
+        console.log("受信したデータ:", items);
+      }
     });
   }, []);
 
-  function createOrder() {
-    client.models.Order.create({ 
-      storeId:'019',
+  function createOrder() { 
+    // 0〜999の乱数を作って3桁のゼロパディングで文字列にする
+    const randomId = Math.floor(Math.random() * 1000);
+    const storeId = randomId.toString().padStart(3, '0');  // 例: "007", "123", "045"
+
+    const randomCount = Math.floor(Math.random() * 10) + 1; // 1〜10 の整数
+    client.models.Order.create({  //Todoのnewのボタンを押すとこの固定データがDBに書き込まれるようにしています
+      storeId:storeId, //storeIdとorderCountを乱数にしてます
       date:'2025-05-22',
       itemId:"Order content",
-      orderCount:3
+      orderCount:randomCount
     });
   }
 
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createOrder}>+ new</button>
+      <button onClick={createOrder}>+ new</button> //orderデータを生成
       <ul>
-        {todos.map((order) => (
-          <li key={order.storeId}>{order.storeName}</li>
+        {orders.map((order) => ( //データの表示部分
+          <li key={order.storeId}>
+            storeId: {order.storeId}, itemId: {order.itemId}, count: {order.orderCount}
+          </li>
         ))}
       </ul>
       <div>
