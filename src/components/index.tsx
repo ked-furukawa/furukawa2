fe94ba03b3b03ec8beff20049c928db2b131e089
trust-ryu <ryu_sakurai@trust-coms.com>
2025-05-26 14:02:39 +0900

    hooksとcomponentsに分離

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,7 @@
 export { StoreHeader } from './StoreHeader';
 export { ProductList } from './ProductList';
 export { Keypad } from './Keypad';
+export * from './ErrorMessage';
+export * from './ConfirmationDialog';
 
 // 必要に応じて、将来的に追加するコンポーネントもここでエクスポートできます
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+// src/hooks/index.ts
+export * from './useStoreManagement';
+export * from './useProductSelection';
