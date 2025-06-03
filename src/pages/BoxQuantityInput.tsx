bad8d640a62db7cd81da333defd8501e1a4e590d
trust-ryu <ryu_sakurai@trust-coms.com>
2025-06-03 11:40:25 +0900

    Order追加

--- src/pages/BoxQuantityInput.tsx
+++ src/pages/BoxQuantityInput.tsx
@@ -24,6 +24,8 @@ type BoxColor = 'green' | 'red' | 'blue' | 'yellow';
 // Amplify クライアントの生成
 const dataClient = generateClient<Schema>();
 
+type Order = Schema['Order']['type'];
+
 // 型定義
 interface Store {
   storeId: string;
