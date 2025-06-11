281d112a59eeab0c5a44874a3ca2f63f06fb7a9a
tk-kurosawa <tk_kurosawa@trust-coms.com>
2025-06-11 09:33:13 +0900

    テスト用に作った関数の名称変更 csvをDBに入れる関数用のファイル作成

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { csvToDB } from './functions/excelToDB/resource';
+import { PUTeventTest } from './functions/PUTeventTest/resource';
 
 import { EventType } from 'aws-cdk-lib/aws-s3';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
@@ -11,10 +11,10 @@ const backend = defineBackend({
 	auth,
 	data,
 	storage,
-	csvToDB
+	PUTeventTest
 });
 
 backend.storage.resources.bucket.addEventNotification(
 	EventType.OBJECT_CREATED_PUT,
-	new LambdaDestination(backend.csvToDB.resources.lambda),
+	new LambdaDestination(backend.PUTeventTest.resources.lambda),
 );
